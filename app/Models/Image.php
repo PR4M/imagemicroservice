6df2d14e79c1a77f6e6161546754883c5797dcf5
pramadiputra <pramadiputra@outlook.com>
2018-05-28 11:32:03 +0800

    Create Model for storing Image

---
+++ app/Models/Image.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pramana
+ * Date: 5/28/2018
+ * Time: 11:26 AM
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+
+}
